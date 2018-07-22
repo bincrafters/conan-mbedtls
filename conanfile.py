9e9fef6db94ae8753ac24d5975a3062c2ab8dddf
Jerry W <jerrywiltse@gmail.com>
2018-07-21 23:32:57 -0400

    add testing/2.11.0 branch and refresh recipe

--- conanfile.py
+++ conanfile.py
@@ -7,38 +7,28 @@ import os
 
 class MbedTLS(ConanFile):
     name = "mbedtls"
-    version = "2.6.1"
+    version = "2.11.0"
     description = "An open source, portable, easy to use, readable and flexible SSL library "
     url = "https://github.com/bincrafters/conan-mbedtls"
     license = "Apache-2.0"
     exports = ["LICENSE.md"]
     generators = "cmake"
-    exports_sources = ["CMakeLists.txt", "patches/library-CMakeLists.txt.patch"]
+    exports_sources = ["CMakeLists.txt"]
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False] }
     default_options = "shared=False"
 
+    source_subfolder = "source_subfolder"
+    build_subfolder = "build_subfolder"
+
     def source(self):
         source_url = "https://github.com/ARMmbed/mbedtls"
-        archive_file = '{0}-{1}.tar.gz'.format(self.name, self.version)
-        source_file = '{0}/archive/{1}'.format(source_url, archive_file)
+        tools.get("{0}/archive/{1}-{2}.tar.gz".format(source_url, self.name, self.version))
+        # Not a mistake, name is actually mbedtls-mbedtls-version
+        extracted_dir = '{0}-{0}-{1}'.format(self.name, self.version)
+        os.rename(extracted_dir, self.source_subfolder)
 
-        tools.download(source_file, archive_file)
-        tools.untargz(archive_file)
-
-        # in 2.6.1 there is a problem with the dir extracted,
-        # it is mbedtls-mbedtls-2.6.1 instead of mbedtls-2.6.1
-        os.rename('{0}-{0}-{1}'.format(self.name, self.version), 'sources')
-
-    def build(self):
-
-        if self.settings.os == "Windows":
-            old_lib_cmake = os.path.join("sources", "library", "CMakeLists.txt")
-            new_lib_cmake = os.path.join("patches", "library-CMakeLists.txt.patch")
-            os.unlink(old_lib_cmake)
-            os.rename(new_lib_cmake, old_lib_cmake)
-
-
+    def configure_cmake(self):
         cmake = CMake(self)
         cmake.verbose = True
         cmake.definitions["ENABLE_TESTING"] = "Off"
@@ -47,40 +37,26 @@ class MbedTLS(ConanFile):
         if self.settings.os == "Windows" and self.options.shared:
             cmake.definitions["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = "On"
 
-        if self.settings.compiler == 'Visual Studio':
-            cmake.definitions["CMAKE_C_FLAGS"] = "-DMBEDTLS_PLATFORM_SNPRINTF_MACRO=snprintf"
-            cmake.definitions["CMAKE_CXX_FLAGS"] = "-DMBEDTLS_PLATFORM_SNPRINTF_MACRO=snprintf"
-
-        #if self.settings.compiler == 'gcc':
-        #    if self.settings.arch == 'x86':
-        #        cmake.definitions["CMAKE_C_FLAGS"] = "-m32"
-        #        cmake.definitions["CMAKE_CXX_FLAGS"] = "-m32"
-        #    else:
-        #        cmake.definitions["CMAKE_C_FLAGS"] = "-m64"
-        #        cmake.definitions["CMAKE_CXX_FLAGS"] = "-m64"
-        
+        # if self.settings.compiler == 'Visual Studio':
+            # cmake.definitions["CMAKE_C_FLAGS"] = "-DMBEDTLS_PLATFORM_SNPRINTF_MACRO=snprintf"
+            # cmake.definitions["CMAKE_CXX_FLAGS"] = "-DMBEDTLS_PLATFORM_SNPRINTF_MACRO=snprintf"
+       
         cmake.definitions["USE_SHARED_MBEDTLS_LIBRARY"] = self.options.shared
         cmake.definitions["USE_STATIC_MBEDTLS_LIBRARY"] = not self.options.shared
-
-        cmake.configure(source_dir="..", build_dir="build")
+        cmake.configure(build_folder=self.build_subfolder)
+        return cmake
+    
+    def build(self):
+        cmake = self.configure_cmake()
         cmake.build()
-        cmake.install()
         
     def package(self):   
-        self.copy("*.h", dst="include", src=os.path.join(self.name, 'include'), keep_path=True)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False, symlinks=True)
-                
-        if self.options.shared:
-            self.copy("*.so*", dst="lib", keep_path=False, symlinks=True)
-        else:
-            self.copy("*.a", dst="lib", keep_path=False, symlinks=True)
-            
-        self.copy("*.lib", dst="lib", keep_path=False, symlinks=True)
-
+        cmake = self.configure_cmake()
+        cmake.install()
+        
     def package_info(self):
         # the order below matters. If changed some linux builds may fail.
-        self.cpp_info.libs = [ 'mbedx509', 'mbedcrypto', 'mbedtls' ]
+        self.cpp_info.libs = [ 'mbedtls', 'mbedx509', 'mbedcrypto' ]
 
         if self.settings.os == "Windows":
             self.cpp_info.defines.append("MBEDTLS_PLATFORM_SNPRINTF_MACRO=snprintf")
