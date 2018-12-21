676a9b6a989c52b8c8bf34df43359302ddd146f7
SSE4 <tomskside@gmail.com>
2018-12-21 23:34:56 +0700

    - update to 2.15.1
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ import os
 
 class MbedTLSConan(ConanFile):
     name = "mbedtls"
-    version = "2.13.0"
+    version = "2.15.1"
     description = "An open source, portable, easy to use, readable and flexible SSL library "
     url = "https://github.com/bincrafters/conan-mbedtls"
     homepage = "https://github.com/ARMmbed/mbedtls"
@@ -18,10 +18,10 @@ class MbedTLSConan(ConanFile):
     exports_sources = ["CMakeLists.txt"]
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
+    default_options = {"shared": False, "fPIC": True}
 
-    source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
 
     def config_options(self):
         if self.settings.os == 'Windows':
@@ -32,12 +32,12 @@ class MbedTLSConan(ConanFile):
         tools.get("{0}/archive/{1}-{2}.tar.gz".format(source_url, self.name, self.version))
         # Not a mistake, name is actually mbedtls-mbedtls-version
         extracted_dir = '{0}-{0}-{1}'.format(self.name, self.version)
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, self._source_subfolder)
 
-        tools.replace_in_file(os.path.join(self.source_subfolder, 'CMakeLists.txt'),
+        tools.replace_in_file(os.path.join(self._source_subfolder, 'CMakeLists.txt'),
                               'set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /WX")', '')
 
-        x509_crt_h = os.path.join(self.source_subfolder, 'include', 'mbedtls', 'x509_crt.h')
+        x509_crt_h = os.path.join(self._source_subfolder, 'include', 'mbedtls', 'x509_crt.h')
         tools.replace_in_file(x509_crt_h,
                               '#if defined(MBEDTLS_X509_CRT_PARSE_C)',
                               '#if defined(MBEDTLS_X509_CRT_PARSE_C)\n'
@@ -61,34 +61,37 @@ class MbedTLSConan(ConanFile):
         tools.replace_in_file(x509_crt_h,
                               'extern const mbedtls_x509_crt_profile mbedtls_x509_crt_profile_suiteb;',
                               'X509_EXPORT const mbedtls_x509_crt_profile mbedtls_x509_crt_profile_suiteb;')
-        tools.replace_in_file(os.path.join(self.source_subfolder, 'include', 'mbedtls', 'bn_mul.h'),
+        tools.replace_in_file(os.path.join(self._source_subfolder, 'include', 'mbedtls', 'bn_mul.h'),
                               '#if defined(__i386__) && defined(__OPTIMIZE__)',
                               '#if defined(__i386__) && defined(__OPTIMIZE__) && !defined(__PIC__)')
+        tools.replace_in_file(os.path.join(self._source_subfolder, 'library', 'CMakeLists.txt'),
+                              '${CMAKE_SOURCE_DIR}/include/',
+                              '${CMAKE_CURRENT_SOURCE_DIR}/../include/')
 
-    def configure_cmake(self):
+    def _configure_cmake(self):
         cmake = CMake(self)
         cmake.verbose = True
-        cmake.definitions["ENABLE_TESTING"] = "Off"
-        cmake.definitions["ENABLE_PROGRAMS"] = "Off"
+        cmake.definitions["ENABLE_TESTING"] = False
+        cmake.definitions["ENABLE_PROGRAMS"] = False
         if self.settings.os != 'Windows':
             cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
 
         if self.settings.os == "Windows" and self.options.shared:
-            cmake.definitions["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = "On"
+            cmake.definitions["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
 
         cmake.definitions["USE_SHARED_MBEDTLS_LIBRARY"] = self.options.shared
         cmake.definitions["USE_STATIC_MBEDTLS_LIBRARY"] = not self.options.shared
-        cmake.configure(build_folder=self.build_subfolder)
+        cmake.configure(build_folder=self._build_subfolder)
         return cmake
 
     def build(self):
-        cmake = self.configure_cmake()
+        cmake = self._configure_cmake()
         cmake.build()
         
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
-        self.copy(pattern="apache-2.0.txt", dst="licenses", src=self.source_subfolder)
-        cmake = self.configure_cmake()
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="apache-2.0.txt", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
         cmake.install()
         
     def package_info(self):
